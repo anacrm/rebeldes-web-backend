a3af74ae8dd1d331ad744a9e9c05aea0674e14b0
Ana Carolina <reismedeiros.ana@gmail.com>
2020-07-01 09:16:27 -0300

    first commit

---
+++ src/main/java/com/anacrm/rebeldeswebbackend/ProjetoRebeldesPhoebusApplication.java
@@ -0,0 +1,13 @@
+package com.anacrm.rebeldeswebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRebeldesPhoebusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRebeldesPhoebusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anacrm/rebeldeswebbackend/ProjetoRebeldesPhoebusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anacrm.rebeldeswebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRebeldesPhoebusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
